851e133ecbb6f0372b49e48b2549f3faa946e459
derr0310 <danielriscorivera@gmail.com>
2025-05-30 01:23:26 -0500

    Actualización con mejoras en proyectos y footer

---
+++ src/data/proyectos.js
@@ -0,0 +1,5 @@
+export const proyectos = [
+  { nombre: "Proyecto 1", imagen: "/assets/proyecto1.jpg", descripcion: "Breve info", tecnologias: "HTML, CSS", link: "#" },
+  { nombre: "Proyecto 2", imagen: "/assets/proyecto2.jpg", descripcion: "Breve info", tecnologias: "React, Astro", link: "#" },
+  { nombre: "Proyecto 3", imagen: "/assets/proyecto3.jpg", descripcion: "Breve info", tecnologias: "Tailwind, Node.js", link: "#" }
+];
